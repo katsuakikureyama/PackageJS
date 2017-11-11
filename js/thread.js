a252c6b1b186872a9da37fe92a62a8e3c3e89691
USER <user@USER-no-MacBook-Pro.local>
2017-11-11 18:53:53 +0900

    package thread

---
+++ js/thread.js
@@ -0,0 +1,23 @@
+/**
+PackageJS
+
+Copyright (c) 2017 katsuakikureyama
+
+This software is released under the MIT License.
+http://opensource.org/licenses/mit-license.php
+*/
+
+package.lang.thread = {};
+package.lang.thread.worker = function(){
+	var task_url ;
+	return js.lang.create ( function (public){
+ 		public.entry = function(task){ 
+ 		    var s = task.toSource();
+    		var b = new Blob( [ s ], {type : "text/javascript"} );
+  	         task_url = window.URL.createObjectURL( b );		
+    	}
+ 	 public.run = function(tag){ 
+ 	     	return new Worker(task_url ); 		   
+	 	}
+ 	} );
+}();
